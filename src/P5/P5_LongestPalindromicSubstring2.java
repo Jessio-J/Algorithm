52a24f6b9362c846648968a9ced118e606feb176
jessio <jzhaha02@126.com>
2019-02-24 15:08:09 +0800

    P5两种解法

---
+++ src/P5/P5_LongestPalindromicSubstring2.java
@@ -0,0 +1,30 @@
+package P5;
+
+public class P5_LongestPalindromicSubstring2 {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            //奇数中心
+            int len1 = expandAroundCenter(s, i, i);
+            //偶数中心
+            int len2 = expandAroundCenter(s, i, i + 1);
+            //取较长串
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
