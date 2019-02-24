eda84932b2f27687533a1ba8732f36e78263af76
jessio <jzhaha02@126.com>
2019-02-24 15:08:37 +0800

    公共的算法执行类

---
+++ src/excuteCenter.java
@@ -0,0 +1,7 @@
+import P5.P5_LongestPalindromicSubstring2;
+
+public class excuteCenter {
+    public static void main(String[] args){
+        System.out.println(""+new P5_LongestPalindromicSubstring2().longestPalindrome("abcbacc"));
+    }
+}
