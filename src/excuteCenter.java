e5bf32cb5fa83281dbf7ef30f637d0d565ae9694
jessio <jzhaha02@126.com>
2019-02-26 10:12:51 +0800

    P6的一种解法

--- src/excuteCenter.java
+++ src/excuteCenter.java
@@ -1,7 +1,9 @@
 import P5.P5_LongestPalindromicSubstring2;
+import P6.P6_ZigZagConversion;
 
 public class excuteCenter {
     public static void main(String[] args){
-        System.out.println(""+new P5_LongestPalindromicSubstring2().longestPalindrome("abcbacc"));
+        System.out.println(""+new P6_ZigZagConversion().convert("LEETCODEISHIRING",4));
+
     }
 }
