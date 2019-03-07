bcc641153aa9266cca24fc531ba6f86b0854bdba
jessio <jzhaha02@126.com>
2019-03-07 15:50:37 +0800

    P9的两种解法

--- src/excuteCenter.java
+++ src/excuteCenter.java
@@ -1,4 +1,6 @@
+import P10.P10_ContainerWithMostWater;
 import P8.P8_StringToInteger2;
+import P9.P9_PalindromeNumber;
 
 public class excuteCenter {
     public static void main(String[] args){
@@ -11,8 +13,9 @@ public class excuteCenter {
 //        int plus = '+';
 //        String[] array = a.split(" ");
 //        System.out.println("1:"+one+"\n0:"+zero+"\n9:"+nine+"\n-:"+minors+"\nblank:"+blank+"\nplus:"+plus);
-
-        System.out.println("answer:"+new P8_StringToInteger2().myAtoi("s1"));
+//        byte[] buffer = new byte[]{0x0A};
+//        int percent = Integer.valueOf("" + (buffer[0]&0xff) ,10);
+        System.out.println("xxx:"+new P10_ContainerWithMostWater().maxArea(new int[]{2,3,10,5,7,8,9}));
 
     }
 }
