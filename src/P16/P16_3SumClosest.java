02559a8dd90d5be6822c75e8c446bfc1afac0942
jessio <jzhaha02@126.com>
2019-03-15 20:55:41 +0800

    P16双指针解法

---
+++ src/P16/P16_3SumClosest.java
@@ -0,0 +1,39 @@
+package P16;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class P16_3SumClosest {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closest = Integer.MAX_VALUE;
+        int sum = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {  // 跳过可能重复的答案
+
+                int l = i + 1, r = nums.length - 1, perfectSum = target - nums[i];
+                while (l < r) {
+                    if (nums[l] + nums[r] == perfectSum) {
+                        return target;
+                    } else if (nums[l] + nums[r] < perfectSum) {
+                        while (l < r && nums[l] == nums[l + 1]) l++;   // 跳过重复值
+                        if(closest > Math.abs(nums[l] + nums[r] - perfectSum)){
+                            closest = Math.abs(nums[l] + nums[r] - perfectSum);
+                            sum = nums[l] + nums[r] + nums[i];
+                        }
+                        l++;
+                    } else {
+                        while (l < r && nums[r] == nums[r - 1]) r--;
+                        if(closest > Math.abs(nums[l] + nums[r] - perfectSum)){
+                            closest = Math.abs(nums[l] + nums[r] - perfectSum);
+                            sum = nums[l] + nums[r] + nums[i];
+                        }
+                        r--;
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+}
