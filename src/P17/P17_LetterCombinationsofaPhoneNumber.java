7ae5f89950d7934907f9b1eff6aa9413b17e3d5a
jessio <jzhaha02@126.com>
2019-03-16 10:28:59 +0800

    P17分治思路求解，补充题目内容

--- src/P17/P17_LetterCombinationsofaPhoneNumber.java
+++ src/P17/P17_LetterCombinationsofaPhoneNumber.java
@@ -2,7 +2,16 @@ package P17;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+//
+//        给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+//
+//
+//
+//        示例:
+//
+//        输入："23"
+//        输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 public class P17_LetterCombinationsofaPhoneNumber {
     public List<String> letterCombinations(String digits) {
         if (digits == null || digits.length() == 0) return new ArrayList<String>();
