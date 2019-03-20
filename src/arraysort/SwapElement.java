d76dc77dff49f7a71dbac7b1c6402d8028aea72e
jessio <jzhaha02@126.com>
2019-03-20 15:19:39 +0800

    swap方法

---
+++ src/arraysort/SwapElement.java
@@ -0,0 +1,9 @@
+package arraysort;
+
+public class SwapElement {
+    public static void swap(int[] arr,int i,int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
