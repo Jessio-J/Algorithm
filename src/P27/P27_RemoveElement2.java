8ccd0f153c39d91bd5b55a72ae4770fdf094551f
jessio <jzhaha02@126.com>
2019-03-22 15:57:55 +0800

    P27三种双指针法求解问题

---
+++ src/P27/P27_RemoveElement2.java
@@ -0,0 +1,14 @@
+package P27;
+//思路：快慢指针法，j是顺序遍历的快指针，i是慢指针，当nums[ j ] !=val 时，把nums[ j ] 赋值给nums[ i ],直到完成遍历。
+public class P27_RemoveElement2 {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
