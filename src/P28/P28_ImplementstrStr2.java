203ac4da8e7fc9b3ad7e7e000cbe24fb6f9a0889
jessio <jzhaha02@126.com>
2019-03-22 16:26:54 +0800

    P28两种暴力解法

---
+++ src/P28/P28_ImplementstrStr2.java
@@ -0,0 +1,26 @@
+package P28;
+//思路：利用字符串的特性解题
+public class P28_ImplementstrStr2 {
+    public int strStr(String haystack, String needle) {
+        if ((haystack == null) || (needle == null)) {
+            return -1;
+        } else {
+            int lengthH = haystack.length();
+            int lengthN = needle.length();
+            if (lengthN == 0) {
+                return 0;
+            } else if (lengthH >= lengthN) {
+                for (int i = 0; i <= (lengthH - lengthN); i++) {
+                    if (haystack.charAt(i) == needle.charAt(0)) {
+                        String temp = haystack.substring(i, i + lengthN);
+                        if (needle.equals(temp)) {
+                            return i;
+                        }
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
