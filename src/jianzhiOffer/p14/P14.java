bf4332f363f6ceb1d465724ad8db0c41cb110f37
jessio <jzhaha02@126.com>
2019-05-05 16:16:18 +0800

    P14动态规划和贪心算法

---
+++ src/jianzhiOffer/p14/P14.java
@@ -0,0 +1,27 @@
+package jianzhiOffer.p14;
+//动态规划
+public class P14 {
+    public int maxProductCutting(int length){
+        if(length<2)
+            return 0;
+        if (length==2)
+            return 1;
+        if(length==3)
+            return 2;
+        int[] p = new int[length+1];
+        p[0] = 0;
+        p[1] = 1;
+        p[2] = 2;
+        p[3] = 3;
+        for(int i=4;i<=length;i++){
+            int max = 0;
+            for(int j = 1;j<(i>>1);j++){
+                int pt = p[j]*p[i-j];
+                if(max<pt)
+                    max = pt;
+            }
+            p[i] = max;
+        }
+        return p[length];
+    }
+}
---
+++ src/jianzhiOffer/p14/P14_2.java
@@ -0,0 +1,21 @@
+package jianzhiOffer.p14;
+//贪心算法
+public class P14_2 {
+    public int maxProductCutting(int length){
+        if(length<2)
+            return 0;
+        if (length==2)
+            return 1;
+        if(length==3)
+            return 2;
+        //尽可能多地剪去长度为3的绳子
+        int timesOf3 = length/3;
+
+        //当绳子剩下长度为4的时候，不能剪3，而应该剪成2X2的
+        if(length-timesOf3*3==1){
+            timesOf3--;
+        }
+        int timesOf2 = (length-timesOf3*3)/2;
+        return (int)Math.pow(3,timesOf3)*(int)Math.pow(2,timesOf2);
+    }
+}
