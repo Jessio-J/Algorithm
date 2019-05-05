9028663dad95833a9fa9b2051b4e87fed28a110a
jessio <jzhaha02@126.com>
2019-05-05 15:09:27 +0800

    P13回溯法求运动范围

---
+++ src/jianzhiOffer/P13/P13.java
@@ -0,0 +1,27 @@
+package jianzhiOffer.P13;
+
+public class P13 {
+
+    public int movingCount(int threshold, int rows, int cols) {
+        int flag[][] = new int[rows][cols]; //记录是否已经走过
+        return helper(0, 0, rows, cols, flag, threshold);
+    }
+
+    private int helper(int i, int j, int rows, int cols, int[][] flag, int threshold) {
+        if (i < 0 || i >= rows || j < 0 || j >= cols || flag[i][j] == 1|| numSum(i) + numSum(j)  > threshold ) return 0;
+        flag[i][j] = 1;
+        return helper(i - 1, j, rows, cols, flag, threshold)
+                + helper(i + 1, j, rows, cols, flag, threshold)
+                + helper(i, j - 1, rows, cols, flag, threshold)
+                + helper(i, j + 1, rows, cols, flag, threshold)
+                + 1;
+    }
+
+    private int numSum(int i) {
+        int sum = 0;
+        do{
+            sum += i%10;
+        }while((i = i/10) > 0);
+        return sum;
+    }
+}
