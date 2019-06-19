86a800a2ebe363b074f6f46ea207bd06a04d23ea
jessio <jzhaha02@126.com>
2019-06-19 20:33:56 +0800

    P120动态规划解法

---
+++ src/P120/P120_2.java
@@ -0,0 +1,15 @@
+package P120;
+
+import java.util.List;
+//动态规划，从倒数第二层到顶层
+public class P120_2 {
+    int minimumTotal(List<List<Integer>> triangle) {
+        for (int i = triangle.size() - 2; i >= 0; i--)
+            for (int j = 0; j < triangle.get(i).size(); j++){
+                int tmp = triangle.get(i).get(j) + Math.min(triangle.get(i+1).get(j), triangle.get(i+1).get(j+1));
+                List<Integer> tmpList = triangle.get(i);
+                tmpList.set(j,tmp);
+            }
+        return triangle.get(0).get(0);
+    }
+}
