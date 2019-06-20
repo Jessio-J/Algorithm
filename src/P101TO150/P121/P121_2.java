a152d5deaf540763f693e08764291ba650d9c9a5
jessio <jzhaha02@126.com>
2019-06-20 10:22:47 +0800

    P121动态规划解股票差价

---
+++ src/P101TO150/P121/P121_2.java
@@ -0,0 +1,14 @@
+package P101TO150.P121;
+//DP动态规划思想
+public class P121_2 {
+    public int maxProfit(int[] prices) {
+        if(prices.length <= 1)
+            return 0;
+        int min = prices[0], max = 0;
+        for(int i = 1; i < prices.length; i++) {
+            max = Math.max(max, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return max;
+    }
+}
