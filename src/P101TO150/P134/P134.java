ecf6b92e2c18baca64d594094e52b2f32f423ce8
Jessio-J <jzhaha02@126.com>
2019-06-28 16:16:38 +0800

    P134加油站

---
+++ src/P101TO150/P134/P134.java
@@ -0,0 +1,16 @@
+package P101TO150.P134;
+
+public class P134 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int rest = 0, run = 0, start = 0;
+        for (int i = 0; i < gas.length; ++i){
+            run += (gas[i] - cost[i]);
+            rest += (gas[i] - cost[i]);
+            if (run < 0){
+                start = i + 1;
+                run = 0;
+            }
+        }
+        return rest < 0 ? -1: start;
+    }
+}
